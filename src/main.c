0a42dd6ba923b1386eeb81422e9bb646e397be10
abdullahjaved06 <javedabdullah963@gmail.com>
2025-05-02 13:09:39 +0500

    added DT node

--- src/main.c
+++ src/main.c
@@ -3,9 +3,14 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/device.h>
+
+static const struct i2c_dt_spec mcp3421_dev = I2C_DT_SPEC_GET(DT_NODELABEL(mcp3421));
+
 
 int main(void)
 {
+	int ret;
+	
 
-	return 0;
 }

44e8ca922a6d034c65b9bd5c2a2ffa0a35126c36
abdullahjaved06 <javedabdullah963@gmail.com>
2025-05-02 13:10:50 +0500

    added logging

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/device.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(MCP3421);
 
 static const struct i2c_dt_spec mcp3421_dev = I2C_DT_SPEC_GET(DT_NODELABEL(mcp3421));
 
@@ -12,5 +15,4 @@ int main(void)
 {
 	int ret;
 	
-
 }
